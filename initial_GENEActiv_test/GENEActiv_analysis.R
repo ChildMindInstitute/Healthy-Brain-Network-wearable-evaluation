6388bcb1dca8839e263fcf4f8cc53fee2f167a5f
Jon Clucas <jon.clucas@childmind.org>
2017-02-28 18:21:23 -0500

    `acc_df` function
    function written to combine acc data into R data.frames.
    
    Documentation also written, but RStudio was still running the code to generate the markdown when I was ready to leave work for the day.
    
    I left RStudio running, so hopefully the documentation will go up tomorrow.

--- initial_GENEActiv_test/GENEActiv_analysis.R
+++ initial_GENEActiv_test/GENEActiv_analysis.R
@@ -1,7 +1,7 @@
 Encoding("UTF-8")
 # GENEActiv_analysis.R
 # 
-# Script to analyze output CSV files from GENEActive wearable devices.
+# Script to analyze output binary files from GENEActive wearable devices.
 # Built in R 3.3.2 (Sincere Pumpkin Patch)
 # 
 # Author:
@@ -47,6 +47,7 @@ require(GGIR)
 
 # dependencies
 require(mmap)
+require(lubridate)
 
 # GENEAread function `read.bin` returns an AccData object
 # AccData factors for the device we are testing are:
@@ -63,12 +64,73 @@ arno_hand <- read.bin(g_files[3])
 curt_hand <- read.bin(g_files[4])
 jon_hand <- read.bin(g_files[5])
 
-AccDatas <- c('arno_foot_l', 'arno_foot_r', 'arno_hand', 'curt_hand', 'jon_hand')
+AccDatas <- c('arno_foot_l', 'arno_foot_r', 'arno_hand', 'curt_hand',
+              'jon_hand')
 processing_times <- c(2.219 * 60, 3.077 * 60, 3.916, 12.753 * 60, 3.57)
 pt2 <- c(39.367, 25.765, 3.652, 9.103*60, 3.991)
 record_frames <- c(14803200, 10368000, 2009100, 33276000, 1728000)
 record_RAM <- c(829, 581, 113, 1863, 97)
 fq <- c(85.7, 60, 20, 100, 10)
 dur <- c(48, 48, 168, 168, 48)
-meta <- data.frame(g_files, AccDatas, fq, dur, processing_times, pt2, record_frames, record_RAM)
+meta <- data.frame(g_files, AccDatas, fq, dur, processing_times, pt2,
+                   record_frames, record_RAM)
 remove(AccDatas, processing_times, pt2, record_frames, record_RAM, fq, dur)
+
+# put all of our data from the same time range into a single dataframe
+acc_df <- function(df, acc, device, start_time, end_time){
+  # function to bind acc data to a dataframe
+  # 
+  # Parameters
+  # ----------
+  # df : dataframe
+  #   the dataframe we'll be binding to
+  # 
+  # acc : AccData
+  #   the acc data we'll be binding
+  #   
+  # device : string
+  #   the name we wnat in the "device" column for this acc data
+  # 
+  # start_time : double (lubridate ymd_hms), optional
+  #   the start_time at which we cut off recording values
+  #   defaults to `ymd_hms("2017-02-20 17:00:00 EST")`
+  #   
+  # end_time : double (lubridate ymd_hms), optional
+  #   the end_time at which we cut off recording values
+  #   defaults to `ymd_hms("2017-02-22 17:00:00 EST")`
+  # 
+  # Returns
+  # -------
+  # df : dataframe
+  #   the original dataframe with the additional acc data appended
+  if(missing(start_time)){
+    start_time = ymd_hms("2017-02-20 17:00:00 EST")
+  }
+  if(missing(end_time)){
+    end_time = ymd_hms("2017-02-22 17:00:00 EST")
+  }
+  accdf <- data.frame(ymd_hms(acc$page.timestamps[ymd_hms(
+           acc$page.timestamps) <= end_time & ymd_hms(acc$page.timestamps) >= 
+           start_time]), acc$x[ymd_hms(acc$page.timestamps) <= end_time & 
+           ymd_hms(acc$page.timestamps) >= start_time], acc$y[ymd_hms(
+           acc$page.timestamps) <= end_time & ymd_hms(acc$page.timestamps) >= 
+           start_time], acc$z[ymd_hms(acc$page.timestamps) <= end_time &
+           ymd_hms(acc$page.timestamps) >= start_time], acc$light[ymd_hms(
+           acc$page.timestamps) <= end_time & ymd_hms(acc$page.timestamps) >= 
+           start_time], acc$button[ymd_hms(acc$page.timestamps) <= end_time & 
+           ymd_hms(acc$page.timestamps) >= start_time], acc$temperature[
+           ymd_hms(acc$page.timestamps) <= end_time & ymd_hms(
+           acc$page.timestamps) >= start_time], c(device))
+  return(rbind(df, accdf))
+}
+# initialize dataframe
+acc_data <- data.frame(timestamp=double(), x=double(), y=double(), z=double(), 
+                       light=double(), button=double(), temperature=double(), device=
+                         factor())
+# append each acc data
+acc_data <- acc_df(acc_data, arno_hand, "Arno hand")
+acc_data <- acc_df(acc_data, arno_foot_l, "Arno left foot")
+acc_data <- acc_df(acc_data, arno_foot_r, "Arno right foot")
+acc_data <- acc_df(acc_data, curt_hand, "Curt hand")
+acc_data <- acc_df(acc_data, jon_hand, "Jon hand")
+names(acc_data) = c('timestamp', 'x', 'y', 'z', 'light', 'button', 'temperature', 'device')
