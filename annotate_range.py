7c8c7cad18f4a6370a2c9c7439e2a88bbe85f92d
Jon Clucas <jon.clucas@childmind.org>
2017-04-17 17:37:23 -0400

    format & update defaults

--- annotate_range.py
+++ annotate_range.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def annotation_line( ax, xmin, xmax, y, text, ytext=0, linecolor='black', linewidth=1, fontsize=12 ):
+def annotation_line(ax, xmin, xmax, text, y=-1.5, ytext=0, linecolor='black',
+                    linewidth=1, fontsize=12):
+    ax.annotate('', xy=(xmin, y), xytext=(xmax, y), xycoords='data',
+                textcoords='data', arrowprops={'arrowstyle': '|-|', 'color':
+                linecolor, 'linewidth':linewidth})
+    ax.annotate('', xy=(xmin, y), xytext=(xmax, y), xycoords='data',
+                textcoords='data', arrowprops={'arrowstyle': '<->', 'color':
+                linecolor, 'linewidth':linewidth})
 
-    ax.annotate('', xy=(xmin, y), xytext=(xmax, y), xycoords='data', textcoords='data',
-            arrowprops={'arrowstyle': '|-|', 'color':linecolor, 'linewidth':linewidth})
-    ax.annotate('', xy=(xmin, y), xytext=(xmax, y), xycoords='data', textcoords='data',
-            arrowprops={'arrowstyle': '<->', 'color':linecolor, 'linewidth':linewidth})
+    xcenter = xmin + (xmax - xmin) / 2
+    if ytext == 0:
+        ytext = y + (ax.get_ylim()[1] - ax.get_ylim()[0]) / 20
 
-    xcenter = xmin + (xmax-xmin)/2
-    if ytext==0:
-        ytext = y + ( ax.get_ylim()[1] - ax.get_ylim()[0] ) / 20
-
-    ax.annotate( text, xy=(xcenter,ytext), ha='center', va='center', fontsize=fontsize)
+    ax.annotate(text, xy=(xcenter,ytext), ha='center', va='center',
+                fontsize=fontsize)
