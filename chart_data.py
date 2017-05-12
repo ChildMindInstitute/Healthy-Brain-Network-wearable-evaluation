818382ef2490f28db7773105816248820fe07941
Jon Clucas <jon.clucas@childmind.org>
2017-05-12 12:42:19 -0400

    fix broken short_test

--- chart_data.py
+++ chart_data.py
@@ -164,58 +164,99 @@ def df_devices(devices, sensor, start, stop, hashes={}):
     """
     suffix = '.csv'
     s = []
-    d = []
+    df = pd.DataFrame()
     for i, device in enumerate(devices):
         if sensor.startswith('acc'):
-            if sensor == 'accelerometer quicktest':
-                d.append(device[1])
-                acc_sub = '_'.join([device[0], 'acc', 'quicktest'])
-            else:
-                d.append(device)
-                acc_sub = '_'.join([device, 'acc'])
-            print(acc_sub)
+            acc_sub = '_'.join([device, 'acc'])
             if not acc_sub in hashes:
                 try:
-                    fetch_check_data(acc_sub, test_urls()[acc_sub], hashes, cache_directory='sample_data',
+                    fetch_check_data(acc_sub, test_urls()[acc_sub], hashes,
+                                     cache_directory='./sample_data',
                                      append='.csv', verbose=True)
                 except:
-                    hashes[acc_sub] = fetch_hash(fetch_data(test_urls()[acc_sub], os.path.join('./sample_data',
-                                          acc_sub), '.csv'))
+                    hashes[acc_sub] = fetch_hash(fetch_data(test_urls()[acc_sub],
+                                      os.path.join('./sample_data', acc_sub), '.csv'))
             s.append(pd.read_csv(fetch_check_data(acc_sub, test_urls()[acc_sub], hashes,
-                     cache_directory='sample_data', append='.csv', verbose=True),
+                     cache_directory='./sample_data', append='.csv', verbose=True),
                      usecols=['Timestamp', 'normalized_vector_length'],
                      parse_dates=['Timestamp'], infer_datetime_format=True, index_col=0,
                      dtype='float'))
-            print(i)
-            print(device)
-            print(len(s))
             if device[1] == 'ActiGraph':
                 s[i].index = pd.Series(s[i].index).apply(lambda x: x - timedelta(microseconds=1000))
-            try:
-                s[i] = s[i].loc[(s[i].index >= start) & (s[i].index <= stop)].copy()
-                s[i] = baseshift_and_renormalize(s[i])
-            except:
-                print(device)
         elif sensor == 'ppg':
             d.append(device)
             ppg = '_'.join([device, sensor])
             if not ppg in hashes:
                 try:
-                    fetch_check_data(device, test_urls()[ppg], hashes, cache_directory='sample_data', append='.csv',
+                    fetch_check_data(device, test_urls()[ppg], hashes,
+                                     cache_directory='sample_data', append='.csv',
                                      verbose=True)
                 except:
-                    hashes[ppg] = fetch_hash(fetch_data(test_urls()[ppg], os.path.join('./sample_data', ppg), '.csv'))
+                    hashes[ppg] = fetch_hash(fetch_data(test_urls()[ppg], os.path.join(
+                                  './sample_data', ppg), '.csv'))
             s.append(pd.read_csv(fetch_check_data(ppg, test_urls()[ppg], hashes,
-                     cache_directory='sample_data', append='.csv', verbose=True), parse_dates=['Timestamp'],
-                     infer_datetime_format=True, index_col=0, dtype='float'))
-            s[i] = s[i].loc[(s[i].index >= start) & (s[i].index <= stop)].copy()
-        if i > 0:
-            if len(s) == 1:
-                df = s[0].merge(s[1], how='outer', left_index=True, right_index=True, suffixes=(''.join([
-                     '_', d[0]]), ''.join(['_', d[1]])))
-            if len(s) > 1:
-                for i in range(2, len(s), 1):
-                    df = df.merge(s[i], how='outer', left_index=True, right_index=True, suffixes=('', ''.join(['_', d[i]])))
+                     cache_directory='sample_data', append='.csv', verbose=True), usecols=[0, 1],
+                     parse_dates=['Timestamp'], infer_datetime_format=True, index_col=0, dtype='float'))
+        s[i] = s[i].loc[(s[i].index >= start) & (s[i].index <= stop)].copy()
+        s[i] = baseshift_and_renormalize(s[i])
+    df = s[0].merge(s[1], how='outer', left_index=True, right_index=True,
+         suffixes=(''.join(['_', d[0]]), ''.join(['_', d[1]])))
+    for i in range(2, len(s), 1):
+        df = df.merge(s[i], how='outer', left_index=True, right_index=True,
+             suffixes=('', ''.join(['_', d[i]])))
+    return(df)
+
+def df_devices_qt(devices, sensor, start, stop, acc_hashes={}):
+    """
+    Function to calculate rolling correlations between two sensor data streams.
+    
+    Parameters
+    ----------
+    devices : list of (subdirectory, device) tuples (len 2)
+        each string is the name of one of the two devices to compare
+        
+    sensor : string
+        the sensor to compare
+        
+    start : datetime
+        beginning of time to compare
+        
+    stop : datetime
+        end of time to compare
+        
+    acc_hashes : dictionary
+        dictionary of cached datafile hashes
+        
+    Returns
+    -------
+    df : pandas dataframe
+        merged dataframe with a column per device
+    """
+    suffix = '.csv'
+    s = []
+    for i, device in enumerate(devices):
+        acc_sub = '_'.join([device[0], 'acc', 'quicktest'])
+        if not acc_sub in acc_hashes:
+            try:
+                fetch_check_data(acc_sub, test_urls()[acc_sub], acc_hashes, cache_directory='./sample_data',
+                                 append='.csv', verbose=True)
+            except:
+                acc_hashes[acc_sub] = fetch_hash(fetch_data(test_urls()[acc_sub], os.path.join('./sample_data',
+                                      acc_sub), '.csv'))
+        s.append(pd.read_csv(fetch_check_data(acc_sub, test_urls()[acc_sub], acc_hashes,
+                 cache_directory='./sample_data', append='.csv', verbose=True),
+                 usecols=['Timestamp', 'normalized_vector_length'],
+                 parse_dates=['Timestamp'], infer_datetime_format=True))
+        s[i] = s[i].loc[(s[i]['Timestamp'] >= start) & (s[i]['Timestamp'] <= stop)].copy()
+        s[i] = baseshift_and_renormalize(s[i])
+        if device[1] == 'ActiGraph':
+            s[i][['Timestamp']] = s[i].Timestamp.apply(lambda x: x - timedelta(microseconds
+                        =1000))
+        s[i].set_index('Timestamp', inplace=True)
+    df = s[0].merge(s[1], left_index=True, right_index=True, suffixes=(''.join([
+         '_', devices[0][1]]), ''.join(['_', devices[1][1]])))
+    for i in range(2, len(s), 1):
+        df = df.merge(s[i], left_index=True, right_index=True, suffixes=('', ''.join(['_', devices[i][1]])))
     return(df)
 
 def linechart(df, plot_label, line=True, full=False):
