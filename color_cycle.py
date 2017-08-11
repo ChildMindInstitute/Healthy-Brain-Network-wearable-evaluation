b6a92202af04b89c3cc5e74662230994508568a8
Jon Clucas <jon.clucas@childmind.org>
2017-08-11 13:02:29 -0400

    Child Mind Institute™ Brand Messaging and Visual Identity Guide | May 2016

---
+++ color_cycle.py
@@ -0,0 +1,19 @@
+from cycler import cycler
+import json
+import os
+with open(os.path.join('Color_palette.json')) as color_palette:
+    color_key = json.load(color_palette)
+
+def CMI_color_palette():
+
+    color_list = list()
+    for palette in ["Primary", "Secondary", "Tertiary"]:
+        color_key[palette].reverse()
+    while(len(color_key)):
+        for palette in ["Primary", "Secondary", "Tertiary"]:
+            if(palette in color_key):
+                color_list.append(color_key[palette].pop())
+                if(not len(color_key[palette])):
+                    del(color_key[palette])
+                
+    return(cycler(c=color_list))
